33e603b98eed0dcb11c489c4d7e2bf6c33bde0b3
asus <1765410986@qq.com>
2019-11-13 23:30:21 +0800

    侧边栏修改

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -7,5 +7,5 @@ from django.http import HttpResponse
 # 视图是接收 httprequest 对象并返回⼀个 httpresponse 对象的Python函数
 def home(request):
     boards = ["33","ww"]#Board.objects.all()
-    return render(request, 'base.html', {'boards': boards})
+    return render(request, 'home.html', {'boards': boards})
     #return HttpResponse('Hello World!')
---
+++ readme.cmd
@@ -0,0 +1,4 @@
+在project中创建app，跳转的manage.py目录下:执行
+django-admin startapp boards
+
+启动命令：python manage.py runserver
